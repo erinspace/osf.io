98432646d48c88ab64e947fcec23bfeb62f50451
erinspace <erin.braswell@gmail.com>
2017-10-20 14:54:48 -0400

    fix listeners

--- addons/osfstorage/listeners.py
+++ addons/osfstorage/listeners.py
@@ -1,11 +1,15 @@
 '''
 Listens for actions to be done to OSFstorage file nodes specifically.
 '''
+
+
 from website.project.signals import contributor_removed
 
 @contributor_removed.connect
 def checkin_files_by_user(node, user):
     ''' Listens to a contributor being removed to check in all of their files
     '''
+    from django.contrib.contenttypes.models import ContentType
     from addons.osfstorage.models import OsfStorageFileNode
-    OsfStorageFileNode.objects.filter(node=node, checkout=user).update(checkout=None)
+    content_type = ContentType.objects.get_for_model(node)
+    OsfStorageFileNode.objects.filter(object_id=node.id, content_type=content_type, checkout=user).update(checkout=None)
